02e895ad145e909cadea30d4d7cdcedcb001ba8e
Paulo Porto <prtpj1@gmail.com>
2021-11-28 16:32:45 -0300

    add Variables.js

---
+++ M1 Fundaments/B4 Intro to JS & Prog Logic/Variable.js
@@ -0,0 +1,10 @@
+const myName = "Paulo";
+const birthCity = "Rio de Janeiro";
+let birthYear = 1982;
+birthYear = 2030
+birthCity = "São Paulo"
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
