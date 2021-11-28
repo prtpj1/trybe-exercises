5d8f0da5bf886b12f57c54d1bce97e976811684c
Paulo Porto <prtpj1@gmail.com>
2021-11-28 17:31:47 -0300

    change exercise5.js

--- M1 Fundaments/B4 Intro to JS & Prog Logic/D1 JavaScript First Steps/exercise5.js
+++ M1 Fundaments/B4 Intro to JS & Prog Logic/D1 JavaScript First Steps/exercise5.js
@@ -1,11 +1,12 @@
 const a = 60;
-const b = 60;
+const b = 30;
 const c = 60;
+const tri = a + b + c
 
 
-if ((a + b + c) === 180) {
+if (tri === 180) {
   console.log(true);
-} else if ((a + b + c) !== 180) {
+} else if (tri > 180 && tri >= 0 ) {
   console.log(false);
 } else {
   console.log("valores inválidos");
