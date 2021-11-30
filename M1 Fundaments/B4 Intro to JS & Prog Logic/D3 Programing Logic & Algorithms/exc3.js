fb98ad6e0848dd6f1124bc1fe6246fc5507235b4
Paulo Porto <prtpj1@gmail.com>
2021-11-30 19:55:19 -0300

    reduce code lines in exc3.js

--- M1 Fundaments/B4 Intro to JS & Prog Logic/D3 Programing Logic & Algorithms/exc3.js
+++ M1 Fundaments/B4 Intro to JS & Prog Logic/D3 Programing Logic & Algorithms/exc3.js
@@ -3,6 +3,5 @@ let aste = "*";
 let espaco = " ";
 
 for (let i = 1; i <= n; i += 1) {
-  let buraco = n - i;
-  console.log(espaco.repeat(buraco) + aste.repeat(i));
+  console.log(espaco.repeat(n - i) + aste.repeat(i));
 }
