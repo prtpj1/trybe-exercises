5a2faedf4c0656d3003d2b6cdc78ad5cd6daa4ff
Paulo Porto <prtpj1@gmail.com>
2021-12-01 15:50:43 -0300

    change exc*.js files to bonus*.js because course update

--- M1 Fundaments/B4 Intro to JS & Prog Logic/D3 Programing Logic & Algorithms/bonus4.js
+++ M1 Fundaments/B4 Intro to JS & Prog Logic/D3 Programing Logic & Algorithms/bonus4.js
@@ -2,6 +2,6 @@ let n = 5;
 let aste = "*";
 let espaco = " ";
 
-for (let i = 1; i <= n; i += 1) {
+for (let i = 1; i < n; i += 1) {
   console.log(espaco.repeat(n - i) + aste.repeat(i + (i - 1)));
 }
