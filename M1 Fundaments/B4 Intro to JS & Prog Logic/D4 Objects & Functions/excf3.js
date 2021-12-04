64dc1a52aed14af253af0e542b774c773dd97144
Paulo Porto <prtpj1@gmail.com>
2021-12-04 18:53:02 -0300

    add excf3.js

---
+++ M1 Fundaments/B4 Intro to JS & Prog Logic/D4 Objects & Functions/excf3.js
@@ -0,0 +1,14 @@
+function  maiorValor(numb) {
+  let n1 = numb[0];
+  let nMenor = [];
+  let mValor = [];
+  for (let i = 0; i < numb.length; i+=1) {
+    if(n1 > numb[i]) {
+      nMenor = numb[i];
+    }
+  }
+  mValor = numb.indexOf(nMenor);
+  console.log(mValor);
+}
+
+maiorValor([2, 4, 6, 7, 10, 0, -3]);
