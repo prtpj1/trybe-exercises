33e2c768ed29089c504fdfe3da53eaea0942cd56
Paulo Porto <prtpj1@gmail.com>
2021-12-10 16:47:31 -0300

    Part 4 Done

--- M1 Fundaments/B5 JS DOM - Events & WebStorage/D3 Events/main.js
+++ M1 Fundaments/B5 JS DOM - Events & WebStorage/D3 Events/main.js
@@ -32,12 +32,11 @@ input.addEventListener('input', function (event) {
   classTech.innerText = event.target.value;
 });
 
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
+myWebpage.addEventListener('dblclick', function() {
+  window.open('https://prtpj1.github.io/');
+});
 
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
-// 4.1. Que tal redirecionar para seu portifólio?
+
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
