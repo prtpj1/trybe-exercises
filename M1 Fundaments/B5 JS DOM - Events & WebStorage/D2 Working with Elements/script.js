97454d03ac2e0d13aa3da087c0ac7ef9628b1b19
Paulo Porto <prtpj1@gmail.com>
2021-12-13 16:28:25 -0300

    Exercise Part IX Done and fix some type errors in other parts

--- M1 Fundaments/B5 JS DOM - Events & WebStorage/D2 Working with Elements/script.js
+++ M1 Fundaments/B5 JS DOM - Events & WebStorage/D2 Working with Elements/script.js
@@ -20,17 +20,35 @@ sectionCenter.appendChild(p);
 
 //Part V
 let sectionLeft = document.createElement('section');
-sectionLeft.className = ('left-content');
-main.appendChild('sectionLeft');
+sectionLeft.className = 'left-content';
+main.appendChild(sectionLeft);
 
 //Part VI
 let sectionRight = document.createElement('section');
-sectionRight.className = ('right-content');
-main.appendChild = (sectionRight);
+sectionRight.className = 'right-content';
+main.appendChild(sectionRight);
 
 //Part VII
-let img = document.createElement('img');
-img.className = ('small-image');
-img.src = 'https://picsum.photos/200';
-sectionLeft.appendChild(img);
+let imgElement = document.createElement('img');
+imgElement.className = 'small-image';
+imgElement.src = 'https://picsum.photos/200';
+sectionLeft.appendChild(imgElement);
 
+
+//Part VIII
+let ui = document.createElement('ul');
+sectionRight.appendChild(ui);
+let numbers = ['Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis',
+'Sete', 'Oito', 'Nove', 'Dez'];
+for (let i in numbers) {
+    let li = document.createElement('li');
+    li.innerHTML = numbers[i];
+    ui.appendChild(li);
+}
+
+//Part IX
+for (let i = 0; i < 3; i+=1){
+    let h3 = document.createElement('h3');
+    h3.innerHTML = 'Show ' + i;
+    main.appendChild(h3);
+}
