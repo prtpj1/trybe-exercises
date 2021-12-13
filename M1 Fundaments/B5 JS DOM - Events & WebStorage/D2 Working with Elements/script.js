5c360b997cc213be8532abfc9dd35cf4e586e8f5
Paulo Porto <prtpj1@gmail.com>
2021-12-13 15:57:54 -0300

    Exercise Part II Done

--- M1 Fundaments/B5 JS DOM - Events & WebStorage/D2 Working with Elements/script.js
+++ M1 Fundaments/B5 JS DOM - Events & WebStorage/D2 Working with Elements/script.js
@@ -3,3 +3,7 @@ let h1 = document.createElement('h1');
 h1.innerText = 'Exercicio 5.2 - Javascript DOM';
 document.body.appendChild(h1);
 
+//Part II
+let main = document.createElement('main');
+main.className = 'main-content';
+document.body.appendChild('main');
