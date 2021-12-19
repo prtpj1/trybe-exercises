bbd68d6ba4f1ebe650222df6b445948f8d8d761f
Paulo Porto <prtpj1@gmail.com>
2021-12-19 17:27:52 -0300

    update D4

---
+++ M1 Fundaments/B5 JS DOM - Events & WebStorage/D4 Web Storage/script.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+    function selectBGColor(color) {
+        let content = document.querySelector('.content');
+        content.style.backgroundColor = color;
+        localStorage.setItem('backgroundColor', color);
+    }
+    let bgColorBtn = document.querySelectorAll(#background)
+}
