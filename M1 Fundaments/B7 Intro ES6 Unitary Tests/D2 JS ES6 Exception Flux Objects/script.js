7aa5d3d1223acd6eddc6eef0c66529f024ceb4b6
Paulo Porto <prtpj1@gmail.com>
2022-01-20 15:51:33 -0300

    Exercise 1 and 2 done

---
+++ M1 Fundaments/B7 Intro ES6 Unitary Tests/D2 JS ES6 Exception Flux Objects/script.js
@@ -0,0 +1,30 @@
+const checkValues = (value1, value2) => {
+  if (!value1 || !value2) {
+    throw new Error(
+      'Precisa informar ambos os valores',
+    );
+  }else if(isNaN(value1) || isNaN(value2)) {
+	throw new Error(
+		'Os valores precisam ser numéricos',
+	  );
+  }
+};
+function sum() {
+  try {
+    const value1 = document.getElementById('value1').value;
+    const value2 = document.getElementById('value2').value;
+    checkValues(value1, value2);
+    const result = parseInt(value1) + parseInt(value2);
+    document.getElementById('result').innerHTML = `Resultado: ${result}`;
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
+  } catch (error) {
+    document.getElementById('result').innerHTML = `Erro: ${error.message}`;
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
+  }
+}
+window.onload = () => {
+  const button = document.getElementById('button');
+  button.addEventListener('click', sum);
+};
