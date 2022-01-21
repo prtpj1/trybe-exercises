f1a84b82d63f54880d7c83c0b9210a0db64ad3bb
Paulo Porto <prtpj1@gmail.com>
2022-01-21 14:16:06 -0300

    Part 3.2 and 3.3 done

--- M1 Fundaments/B7 Intro ES6 Unitary Tests/D2 JS ES6 Exception Flux Objects/part3.js
+++ M1 Fundaments/B7 Intro ES6 Unitary Tests/D2 JS ES6 Exception Flux Objects/part3.js
@@ -1,26 +1,37 @@
 const lesson1 = {
-	materia: 'Matemática',
-	numeroEstudantes: 20,
-	professor: 'Maria Clara',
-	turno: 'manhã',
-  };
-  
-  const lesson2 = {
-	materia: 'História',
-	numeroEstudantes: 20,
-	professor: 'Carlos',
-  };
-  
-  const lesson3 = {
-	materia: 'Matemática',
-	numeroEstudantes: 10,
-	professor: 'Maria Clara',
-	turno: 'noite',
-  };
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
 
-  function chosePeriod(lesson, shift, period){
-	  lesson[shift] = period;
-  };
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function chosePeriod(lesson, shift, period) {
+  lesson[shift] = period;
+}
+
+function listKeys(obj) {
+  const keys = Object.keys(obj);
+  console.log(keys);
+}
+
+function objSize(obj) {
+	const size = Object.keys(obj).length;
+	console.log(size);
+  }
 
 chosePeriod(lesson2, 'turno', 'noite');
-console.log(lesson2);
+listKeys(lesson3);
+objSize(lesson2);
