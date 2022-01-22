30085d5e7875663c3963723b0fcb4b862fa0d375
Paulo Porto <prtpj1@gmail.com>
2022-01-22 14:59:27 -0300

    remove trash from part2.js

--- M1 Fundaments/B7 Intro ES6 Unitary Tests/D3 Jest First Steps/part2.js
+++ M1 Fundaments/B7 Intro ES6 Unitary Tests/D3 Jest First Steps/part2.js
@@ -1,14 +1,11 @@
 function myRemove(arr, item) {
-	let newArr = [];
-	for (let index = 0; index < arr.length; index += 1) {
-	  if (item !== arr[index]) {
-		newArr.push(arr[index]);
-	  }
-	}
-	console.log(newArr);
-	return newArr;
-	
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
   }
-  myRemove([1, 2, 3, 4], 5);
-  
+  return newArr;
+}
+
 module.exports = myRemove;
