8f015e377a39c26bbd21282f8d4e7af6f680e2a4
Paulo Porto <prtpj1@gmail.com>
2022-02-16 19:17:14 -0300

    first steps on React

---
+++ M2 Front-End/D1 Intro to React/classes-react-app/src/Component.js
@@ -0,0 +1,10 @@
+function Component() {
+    return (
+        <div>
+            <h1>Paulo Porto</h1>
+            <p>I'm a person whom usually dont knows what write about himself!</p>
+        </div>
+    );
+}
+
+export default Component;
